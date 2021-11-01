705ce0df8d769a9fa1e9b0bfb1b54a47389c40c1
mayank0508 <thakurmayank0508@gmail.com>
2021-11-01 13:39:09 +0530

    error corrected by installing version 4.14 of sass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Homepage from './homepage.component';
 
+
 function App() {
   return (
     <div>
--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './homepage.styles.scss';
+
 
 const Homepage = () => (
   <div className="homepage">
