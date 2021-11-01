eb7ccdd1b3d076fec8775f1eb3f01c5cf72922a0
mayank0508 <thakurmayank0508@gmail.com>
2021-11-01 13:02:41 +0530

    homepage-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Homepage from './homepage.component';
 
 function App() {
-  return <div> Hello Babylon </div>;
+  return (
+    <div>
+      {' '}
+      <Homepage />{' '}
+    </div>
+  );
 }
 
 export default App;
