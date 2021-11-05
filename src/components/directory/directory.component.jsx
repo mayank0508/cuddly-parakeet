43c012103421f5c580d3aaee0e98b496a801c815
mayank0508 <thakurmayank0508@gmail.com>
2021-11-05 23:47:15 +0530

    directoy work

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      section: []
+    };
+  }
+}
+
+export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -9,7 +9,7 @@ const Homepage = () => (
           <h1 className="title">HATS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
-      </div>
+      </div> 
       <div className="menu-item">
         <div className="content">
           <h1 className="title">JACKETS</h1>
