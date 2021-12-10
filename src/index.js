bd694d5dac05a78d4f2ca887d0a199abc15ca84b
mayank0508 <thakurmayank0508@gmail.com>
2021-12-11 01:37:04 +0530

    react-router-dom added 😲

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render( // this particular wrapping of {browserRouter} will make sure that the app has routing enabled
+  <BrowserRouter> 
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
