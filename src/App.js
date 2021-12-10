a000f6a9f5239a9180b4b9ce62688543ee40b9c4
mayank0508 <thakurmayank0508@gmail.com>
2021-12-11 02:02:06 +0530

    react-router-dom added 😲 but a bit buggy will check it later

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsApp = () => (
+  <div>
+    <h1>HATS APP</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      {' '}
-      <Homepage />{' '}
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsApp} />
+      </Switch>
     </div>
   );
 }
