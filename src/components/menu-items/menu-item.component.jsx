1ea7a7eca9fa9b2ccf4ffd90385e445d684b21e3
mayank0508 <thakurmayank0508@gmail.com>
2021-12-11 01:10:10 +0530

    hover-setting

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -4,13 +4,16 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
   <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
     className={`${size} menu-item`} // this is the wau we declare the imageUrl in the homepage
   >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
