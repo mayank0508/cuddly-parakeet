51c3450a56fdd3d61f1dc3d3aae427262f6a184b
mayank0508 <thakurmayank0508@gmail.com>
2021-12-12 02:18:30 +0530

    data provided

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -3,9 +3,10 @@ import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    className={`${size} menu-item`} onclick={() => history.push()} 
+    className={`${size} menu-item`}
+    onclick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className="background-image"
