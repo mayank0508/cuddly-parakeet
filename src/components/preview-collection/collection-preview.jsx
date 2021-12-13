8898667fd983880fcbc7bb85fabe5b938a5b864b
mayank0508 <thakurmayank0508@gmail.com>
2021-12-14 01:49:14 +0530

    collection-previews

--- src/components/preview-collection/collection-preview.jsx
+++ src/components/preview-collection/collection-preview.jsx
@@ -6,12 +6,13 @@ const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map(item => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      {items
+        .filter((item, idx) => idx < 4) // this will make sure that only 4 items are rendered
+        .map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
     </div>
   </div>
 );
-
 
 export default CollectionPreview;
