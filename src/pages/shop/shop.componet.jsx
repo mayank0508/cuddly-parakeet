75d86974ad1cc3e44bf3247c1a0545eeb059768d
mayank0508 <thakurmayank0508@gmail.com>
2021-12-14 01:12:35 +0530

    shop-data

--- src/pages/shop/shop.componet.jsx
+++ src/pages/shop/shop.componet.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.component';
+import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/preview-collection/collection-preview';
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
