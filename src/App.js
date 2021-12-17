821abdea045e72a909d34dff35b9978c7a025dd5
mayank0508 <thakurmayank0508@gmail.com>
2021-12-18 01:49:33 +0530

    header-componet done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.componet.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
       {/* <Link to="/">Homepage</Link>
       <Link to="/hats">HatsApp/</Link> */}
+      <Header />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/shop" element={<ShopPage />} />
