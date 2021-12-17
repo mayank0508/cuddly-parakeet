5a8caf16ae71c7c4b3c5d2082e3e743c1fa1158e
mayank0508 <thakurmayank0508@gmail.com>
2021-12-18 01:56:05 +0530

    header-componet done

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.component.styles.scss';
+import './header.styles.scss';
 
 const Header = () => (
   <div className="header">
