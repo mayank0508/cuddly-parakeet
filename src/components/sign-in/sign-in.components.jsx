fa1b78e65152bd709ef9786c85a049f280c915dc
mayank0508 <thakurmayank0508@gmail.com>
2021-12-23 00:03:23 +0530

    sign in form made basic

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -18,8 +18,21 @@ class SignIn extends React.Component {
         <h2>Already you have a account</h2>
         <span>Sign with your real email id and password</span>
 
-        <form></form>
+        <form>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+          />
+          <label>Email</label>
+          <input type="submit" value="Subit Form" />
+        </form>
       </div>
     );
   }
 }
+
+export default SignIn;
