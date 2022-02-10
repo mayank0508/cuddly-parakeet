9a2d7900ddff88b4575b7c11820391125a4b00e8
mayank0508 <thakurmayank0508@gmail.com>
2022-02-10 22:59:49 +0530

    sign up

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.componet';
+
+import { auth, createUserProfileDoc } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+}
