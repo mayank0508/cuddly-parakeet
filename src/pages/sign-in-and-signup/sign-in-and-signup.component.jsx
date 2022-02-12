bd191b55ab35f099fe1d4c114cb456ed965213ca
mayank0508 <thakurmayank0508@gmail.com>
2022-02-12 23:44:57 +0530

    sign upand sign in  styling

--- src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
+++ src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './sign-in-and-signup.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignINandSignUP = () => (
   <div className="sign-in-and-signup">
     <SignIn />
+    <SignUp />
   </div>
 );
 
