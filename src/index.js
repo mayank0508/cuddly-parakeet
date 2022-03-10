6879ed4b7eb0ff7b9ae3f9cd3b3d35386e5220b7
mayank0508 <thakurmayank0508@gmail.com>
2022-03-10 14:58:26 +0530

    redux added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( // this particular wrapping of {browserRouter} will make sure that the app has routing enabled
-  <BrowserRouter> 
-    <App />
-  </BrowserRouter>,
+ReactDOM.render(
+  // this particular wrapping of {browserRouter} will make sure that the app has routing enabled
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
   document.getElementById('root')
 );
-
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
